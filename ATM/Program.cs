a87202744e161959376a70507cd5a999266dbbd4
Yun Jang <jang.yunj7@gmail.com>
2023-02-20 23:34:24 -0500

    initial commit

---
+++ ATM/Program.cs
@@ -0,0 +1,16 @@
+﻿using ATM;
+
+void printOptions()
+{
+    Console.WriteLine("Please choose from one of the following...");
+    Console.WriteLine("1. Deposit");
+    Console.WriteLine("2. Withdraw");
+    Console.WriteLine("3. Show Balance");
+    Console.WriteLine("4. Exit");
+}
+
+void deposit(CardHolder currentUser)
+{
+    Console.WriteLine("How much $ would you like to deposit? ");
+    double deposit = double.Parse(Console.ReadLine());
+}
