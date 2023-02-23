b907f75013fae91b0045c4cb13aa68f479c2ae17
Yun Jang <jang.yunj7@gmail.com>
2023-02-22 22:02:35 -0500

    last commit

--- ATM/CardHolder.cs
+++ ATM/CardHolder.cs
@@ -22,3 +22,48 @@ namespace ATM
             LastName = lastName;
             Balance = balance;
         }
+
+
+        public String getNum()
+        {
+            return CardNum;
+        }
+        public int getPIN()
+        {
+            return PIN;
+        }
+        public string getFirstName()
+        {
+            return FirstName;
+        }
+        public string getLastName()
+        {
+            return LastName;
+        }
+        public double getBalance()
+        {
+            return Balance;
+        }
+        public void setNum(string newCardNum)
+        {
+            CardNum = newCardNum;
+        }
+        public void setPIN(int newPIN)
+        {
+            PIN = newPIN;
+        }
+        public void setFirstName(string newFirstName)
+        {
+            FirstName = newFirstName;
+        }
+        public void setLastName(string newLastName)
+        {
+            LastName = newLastName;
+        }
+        public void setBalance(double newBalance)
+        {
+            Balance = newBalance;
+        }
+
+    }
+}
